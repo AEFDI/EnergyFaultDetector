0bda39cb9fe4ba6299a30320b22958825d536493
croelofs <25582572+roelofsc@users.noreply.github.com>
2026-09-02 19:09:41 +0200

    Use Zenodo concept DOI and fix CARE2Compare notebook API calls
    Replace version-specific Zenodo DOIs (14958989, 15846963) with the concept
    DOI 10958774 across docs, source, and notebooks so links always point to
    the latest version. The concept ID resolves correctly via Zenodo's REST
    API, so download_zenodo_data and Care2CompareDataset now default to it.
    
    Fix broken evaluate_event kwargs in the CARE to Compare notebook
    (event_start_id/event_end_id -> event_start/event_end), switch all
    examples to index_column='time_stamp', and update the data directory name
    to CARE_To_Compare.

--- energy_fault_detector/evaluation/care2compare.py
+++ energy_fault_detector/evaluation/care2compare.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('energy_fault_detector')
 class Care2CompareDataset:
     """Loads the Care to Compare Dataset (accompanying paper https://doi.org/10.3390/data9120138).
 
-    The data can be downloaded either manually from https://doi.org/10.5281/zenodo.14958989 (in this case specify
+    The data can be downloaded either manually from https://doi.org/10.5281/zenodo.10958774 (in this case specify
     `path`) or it can be downloaded automatically by setting download_dataset to True.
 
     By default, only the averages are read. See statistics argument of the data loading methods.
@@ -46,7 +46,7 @@ class Care2CompareDataset:
             logger.info("Downloading CARE to Compare dataset (~5 GB) from zenodo. Depending on your internet "
                         "connection this can take some time. Additionally the downloaded zip-file will be unzipped "
                         "(~20 GB) which can also take longer.")
-            path = download_zenodo_data(identifier="10.5281/zenodo.15846963", dest=path, overwrite=False)
+            path = download_zenodo_data(identifier="10.5281/zenodo.10958774", dest=path, overwrite=False)
         self.path: Path = Path(path)
 
         self.wind_farms: Dict[str, Path] = {
